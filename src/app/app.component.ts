14efd955026853c0eedd87792a8b5f54dc529f8c
unknown <hitma1221@gmail.com>
2024-07-16 20:37:30 -0400

    ui Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 interface Background {
   value: string,
@@ -24,29 +25,40 @@ interface Background {
             MatSelectModule,
             MatFormFieldModule,
             MatInputModule,
-            FormsModule],
+            FormsModule,
+            MatToolbarModule],
   template: `
   <main>
-      <header class="brand-name">
-        <!-- <mat-icon aria-hidden="false" aria-label="Example home icon" fontIcon="home"></mat-icon> -->
+    <header>
+      <mat-toolbar class = "toolbar">     
         <a [routerLink]="['/']">
           <button mat-button class="home-button">
             <mat-icon aria-hidden="false" aria-label="Example home icon" fontIcon="home"></mat-icon>
-            Home
           </button>
         </a>
+        <span class="toolbar-spacer"></span>
+        
         <mat-form-field class="background-field">
           <mat-label>Background</mat-label>
-          <mat-select [(ngModel)]="selectedBackground" name="food" (selectionChange)="changeBackground(selectedBackground)">
+          <mat-select [(ngModel)]="selectedBackground" name="Backgrounds" (selectionChange)="changeBackground(selectedBackground)">
             @for (background of backgrounds; track background) {
               <mat-option [value]="background.value">{{background.viewValue}}</mat-option>
             }
           </mat-select>
         </mat-form-field>
-      </header>
+      </mat-toolbar>
+    </header>
     <section class="content">
       <router-outlet></router-outlet>
     </section>
+    <mat-toolbar class="footer">
+      <span class="signature">J. Holden</span>
+      <a mat-button href="https://github.com/FatherOfSaturn" class="github-text">Github</a>
+      <span class="spacer"></span>
+      <span>
+        <a mat-button class="wizards-text">All rights of Magic The Gathering is owned by Wizards</a>
+      </span>
+    </mat-toolbar>
   </main>
   `,
   providers: [
