72c8aa1ce35f5116561e29eb2329252755659395
unknown <hitma1221@gmail.com>
2024-11-03 20:45:11 -0500

    fixing url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://pyramiddraft.xyz:8080/api/game'  // Update to the desired production URL
+  apiUrl: 'http://pyramiddraft.xyz:8080'  // Update to the desired production URL
 };
